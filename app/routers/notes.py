eadebc6f40a832600aec45184c6b0f843e3e2811
Div-30 <divinpaul88@gmail.com>
2026-04-08 18:27:13 +0200

    feat: add patch and delete endpoints

--- app/routers/notes.py
+++ app/routers/notes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Request, status
+from fastapi import APIRouter, HTTPException, Response, status
 from datetime import datetime
 
 from app.database import notes
@@ -13,11 +13,39 @@ router = APIRouter(
 def get_notes():
     return notes
 
-@router.post("/", status_codes=status.HTTP_201_CREATED, response_model=schemas.NoteResponse)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.NoteResponse)
 def create_post(note: schemas.NoteCreate):
     note_dict = note.model_dump()
-    new_id = 1 if not note else max(note["id"] for note in notes) + 1
+    new_id = 1 if not notes else max(n["id"] for n in notes) + 1
 
     note_dict["id"] = new_id
     note_dict["created_at"] = datetime.now()
+
+    notes.append(note_dict)
+    return note_dict
+
+@router.get("/{id}", response_model=schemas.NoteResponse)
+def get_note(id: int):
+    for note in notes:
+        if note["id"] == id:
+            return note
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Note of the {id} is not found")
+
+@router.patch("/{id}", response_model=schemas.NoteResponse)
+def update_note(id: int, updated_note: schemas.NoteUpdate):
+    for note in notes:
+        if note["id"] == id:
+            update_data = updated_note.model_dump(exclude_unset=True)
+            note.update(update_data)
+            return note
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Note of the {id} is not found")
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_note(id: int):
+    for i, note in enumerate(notes):
+        if note["id"] == id:
+            notes.pop(i)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Note of the {id} is not found")
+    
 
