35aa2690ed58569b8cb0435d46202fc1427ff808
Div-30 <divinpaul88@gmail.com>
2026-04-14 09:00:09 +0200

    fix: rebuilt git history and added layered architecture refactor

--- app/routers/notes.py
+++ app/routers/notes.py
@@ -1,8 +1,6 @@
 from typing import Optional
-from fastapi import APIRouter, HTTPException, Response, status
-from datetime import datetime
-
-from app.database import notes
+from fastapi import APIRouter, HTTPException, Response, status, BackgroundTasks
+from app.service import note_service
 from app import schemas
 
 router = APIRouter(
@@ -11,28 +9,28 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=list[schemas.NoteResponse])
-def get_notes(limit: int = 5, skip: int = 0, search: Optional[str] = ""):
-    filtered_notes = [notes for notes in notes if search.lower() in notes['title'].lower()]
-    paginated_notes = filtered_notes[skip : skip + limit]
-    return paginated_notes
+def get_note(limit: int = 5, skip: int = 0, search: Optional[str] = ""):
+    return note_service.filter_and_paginated_notes(search, skip, limit)
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.NoteResponse)
-def create_post(note: schemas.NoteCreate):
+def create_note(note: schemas.NoteCreate, background_tasks: BackgroundTasks):
     note_dict = note.model_dump()
-    new_id = 1 if not notes else max(n["id"] for n in notes) + 1
-
-    note_dict["id"] = new_id
-    note_dict["created_at"] = datetime.now()
-
-    notes.append(note_dict)
+    note_dict["id"] = len(note_service.notes_db) + 1
+    note_service.notes_db.append(note_dict)
+    background_tasks.add_task(
+        note_service.send_email_notification,
+        note_title=note.title,
+        author=note.author
+    )
     return note_dict
 
 @router.get("/{id}", response_model=schemas.NoteResponse)
 def get_note(id: int):
-    for note in notes:
-        if note["id"] == id:
-            return note
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Note of the id {id} is not found")
+    note = note_service.get_note_by_id(id)
+    if not note:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Note of the id {id} is not found")
+    return note
+
 
 @router.patch("/{id}", response_model=schemas.NoteResponse)
 def update_note(id: int, updated_note: schemas.NoteUpdate):
---
+++ app/service/note_service.py
@@ -0,0 +1,28 @@
+notes_db = []
+
+def filter_and_paginated_notes(search: str, limit: int, skip: int):
+    filtered = [note for note in notes_db if search.lower() in notes_db['title'].lower()]
+    return filtered[skip: skip + limit]
+
+# This is a simulated background task
+def send_email_notification(note_title: str, author: str):
+    print(f"Sending email to {author} regarding new note: '{note_title}")
+
+def get_note_by_id(note_id: int):
+    for note in notes_db:
+        if note["id"] == note_id:
+            return note
+    return None
+def update_note_by_id(note_id: int, update_data: dict):
+    for note in notes_db:
+        if note["id"] == note_id:
+            note.update(update_data)
+            return note
+    return None
+
+def delete_note_by_id(note_id: int):
+    for i, note in enumerate(notes_db):
+        if note["id"] == note_id:
+            notes_db.pop(i)
+            return True
+    return False
